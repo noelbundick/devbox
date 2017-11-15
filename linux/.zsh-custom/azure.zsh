172542375c6d218e5eb003ea29e01c9a65e605d6
Noel Bundick <noelbundick@gmail.com>
2017-11-15 18:52:21 +0000

    Silent flag to curl for azmeta

--- linux/.zsh-custom/azure.zsh
+++ linux/.zsh-custom/azure.zsh
@@ -5,7 +5,7 @@ if grep -q unknown-245 /var/lib/dhcp/dhclient.eth0.leases; then
 
   # Aliases
   function azmeta() {
-    curl -H 'Metadata: true' 'http://169.254.169.254/metadata/instance?api-version=2017-04-02' | jq
+    curl -s -H 'Metadata: true' 'http://169.254.169.254/metadata/instance?api-version=2017-04-02' | jq
   }
 
 fi
