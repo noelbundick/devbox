17a93eb53faf286b5b7330f0a078b8913a1fadc2
Noel Bundick <noelbundick@gmail.com>
2018-01-03 08:52:18 -0800

    Resolved merge conflict

--- linux/.zsh-custom/aliases.zsh
+++ linux/.zsh-custom/aliases.zsh
@@ -24,6 +24,15 @@ function dnuke() {
   echo # empty line
 }
 
+# Remove all Docker images
+function dnukeimages() {
+  if read -q '?Are you sure (y/N)?'; then
+    echo # empty line
+    docker rmi --force $(docker images -aq)
+  fi
+  echo # empty line
+}
+
 # SSH agent with default key
 function ssa() {
   eval $(ssh-agent -s)
