0a6ddeaf662089ec3f36f279fb38da6a903f5c00
Noel Bundick <noelbundick@gmail.com>
2018-05-23 14:28:31 -0700

    Add web search aliases Move versions to vars Move clone above tasks to update to the latest version in one shot

--- linux/.zsh-custom/aliases.zsh
+++ linux/.zsh-custom/aliases.zsh
@@ -62,3 +62,30 @@ function nginx-serve() {
 function scratch() {
   (code ~/scratch/`date +%Y-%m-%d`.md </dev/null &>/dev/null &)
 }
+
+function web_search() {
+  typeset -A urls
+  urls=(
+    github  "https://github.com/search?q="
+    google  "https://www.google.com/search?q="
+  )
+
+  if [[ -z "$urls[$1]" ]]; then
+    echo "Search engine $1 not supported"
+    return 1
+  fi
+
+  if [[ $# -gt 1 ]]; then
+    # build search url
+    # join arguments passed with '+', then append to search engine URL
+    url="${urls[$1]}${(j:+:)@[2,-1]}"
+  else
+    # build main page url
+    # split by '/', then rejoin protocol (1) and domain (2) parts with '//'
+    url="${(j://:)${(s:/:)urls[$1]}[1,2]}"
+  fi
+
+  open_command "$url"
+}
+alias github="web_search github"
+alias google="web_search google"
