3b0dfeaced4e260d49be3e540491719209f7aa4a
Noel Bundick <noelbundick@gmail.com>
2018-08-28 13:53:51 -0700

    virtualenv updates + pandoc

--- linux/.zsh-custom/python.zsh
+++ linux/.zsh-custom/python.zsh
@@ -3,3 +3,7 @@ export VIRTUAL_ENV_DISABLE_PROMPT=true
 source ~/.virtualenvs/py3/bin/activate
 alias py2="source ~/.virtualenvs/py2/bin/activate"
 alias py3="source ~/.virtualenvs/py3/bin/activate"
+
+alias venvon="export VIRTUAL_ENV_DISABLE_PROMPT=false"
+alias venvoff="export VIRTUAL_ENV_DISABLE_PROMPT=true"
+
