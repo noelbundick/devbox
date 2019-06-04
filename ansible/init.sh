395fb18f15e95617d1ea73dab5e678fb9d81438d
Noel Bundick <noelbundick@gmail.com>
2019-06-04 08:22:00 -0700

    Add pip install

--- ansible/init.sh
+++ ansible/init.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 # Clone devbox repo
 git clone https://github.com/noelbundick/devbox ~/.devbox
 
+# Install pip
+sudo apt install -y python3-pip
+
 # Install ansible
 export PATH="~/.local/bin:$PATH"
 python3 -m pip install ansible --user
