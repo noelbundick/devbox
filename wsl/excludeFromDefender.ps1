8bc328f02794ba571f02812fb5b12da55e7d2e98
Noel <noelbundick@gmail.com>
2019-06-11 20:36:46 -0700

    update to exclude processes

--- wsl/excludeFromDefender.ps1
+++ wsl/excludeFromDefender.ps1
@@ -7,7 +7,7 @@
 ############
 
 # Find registered WSL environments
-$wslPaths = (Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss | % { Get-ItemProperty $_.PSPath}).BasePath
+$wslPaths = (Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss | ForEach-Object { Get-ItemProperty $_.PSPath}).BasePath
 
 # Get the current Windows Defender exclusion paths
 $currentExclusions = $(Get-MpPreference).ExclusionPath
@@ -16,12 +16,25 @@ if (!$currentExclusions) {
 }
 
 # Find the WSL paths that are not excluded
-$exclusionsToAdd = ((diff $wslPaths $currentExclusions) | ? SideIndicator -eq "<=").InputObject
+$exclusionsToAdd = ((Compare-Object $wslPaths $currentExclusions) | Where-Object SideIndicator -eq "<=").InputObject
+
+# List of paths inside the Linux distro to exclude (https://github.com/Microsoft/WSL/issues/1932#issuecomment-407855346)
+$dirs = @("\bin", "\sbin", "\usr\bin", "\usr\sbin", "\usr\local\bin", "\usr\local\go\bin")
 
 # Add the missing entries to Windows Defender
 if ($exclusionsToAdd.Length -gt 0) {
-  $exclusionsToAdd | % { 
+  $exclusionsToAdd | ForEach-Object { 
+    
+    # Exclude paths from the root of the WSL install
     Add-MpPreference -ExclusionPath $_
     Write-Output "Added exclusion for $_"
+
+    # Exclude processes contained inside WSL
+    $rootfs = $_ + "\rootfs"
+    $dirs | ForEach-Object {
+        $exclusion = $rootfs + $_ + "\*"
+        Add-MpPreference -ExclusionProcess $exclusion
+        Write-Output "Added exclusion for $exclusion"
+    }
   }
 }
