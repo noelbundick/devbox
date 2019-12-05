676ff9cdfb31f6fa00490e655540304e3df71043
Noel <noelbundick@gmail.com>
2019-12-05 11:08:05 -0800

    Workaround for libssl prompt

--- ansible/init.sh
+++ ansible/init.sh
@@ -6,8 +6,12 @@ if [ ! -d ~/.devbox ]; then
     git clone https://github.com/noelbundick/devbox ~/.devbox
 fi
 
+# Hacks for libssl1.1 on Ubuntu 18.04 (https://github.com/hashicorp/vagrant/issues/10914#issuecomment-503055527)
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get -q --option "Dpkg::Options::=--force-confold" --assume-yes install libssl1.1
+
 # Install pip
-sudo apt update
 sudo apt install -y python3-pip
 
 # Install ansible
